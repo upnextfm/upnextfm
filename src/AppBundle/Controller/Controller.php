e5bbf627ea3ebc1761ee3756b893e4cc67c9d8c2
headz <sean@dulotech.com>
2017-08-11 09:51:33 -0400

    Adds RoomController

---
+++ src/AppBundle/Controller/Controller.php
@@ -0,0 +1,8 @@
+<?php
+namespace AppBundle\Controller;
+
+class Controller
+    extends \Symfony\Bundle\FrameworkBundle\Controller\Controller
+{
+
+}
---
+++ src/AppBundle/Controller/RoomController.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * @Route("/r/{name}", name="room")
+     *
+     * @param string $name Name of the room
+     * @return Response
+     */
+    public function indexAction($name)
+    {
+        return $this->render('default/room.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
