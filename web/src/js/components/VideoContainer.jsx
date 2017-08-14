a6b678dd3fa0c9a2b64c2db2706ab28ef801de09
az4521 <cheeze.sprinkels@gmail.com>
2017-08-14 15:30:51 -0400

    fix everything probably in this one bit

--- web/src/js/components/VideoContainer.jsx
+++ web/src/js/components/VideoContainer.jsx
@@ -12,12 +12,10 @@ class VideoContainer extends React.Component {
     };
 
     return (
-      <div className="up-room__video">
-        <Paper elevation = {4} className = "up-room__paper_container">
-          <YouTube videoId="NegV-ts35cY" opts={opts} />
-          <div className = "up-room__video_buttons"><p>BUTTONS GO HERE</p></div>
-        </Paper>
-      </div>
+      <Paper elevation = {4} className = "up-room__paper_container up-room__video">
+        <YouTube videoId="NegV-ts35cY" opts={opts} />
+        <div className = "up-room__video_buttons"><p>BUTTONS GO HERE</p></div>
+      </Paper>
     );
   }
 }
