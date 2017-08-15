711607cb340d2fcc888b2b4496c4ae5182bd7e3e
headz <sean@dulotech.com>
2017-08-15 15:45:15 -0400

    Adds vote skip button

--- web/src/js/components/VideoButtons.jsx
+++ web/src/js/components/VideoButtons.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import IconButton from 'material-ui/IconButton';
+import SkipNext from 'material-ui-icons/SkipNext';
 
 class VideoButtons extends React.Component {
   render() {
     return (
       <div className="up-room__video-buttons up-paper-container">
-        BUTTONS
+        <IconButton title="Vote Skip" aria-label="Vote Skip">
+          <SkipNext />
+        </IconButton>
       </div>
     );
   }
