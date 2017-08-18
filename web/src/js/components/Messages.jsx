93bbe771682ad4f767d5fc3d5316f2e5c2890f8b
headzoo <sean@headzoo.io>
2017-08-18 07:23:56 -0400

    Adds fancy scrollbars

--- web/src/js/components/Messages.jsx
+++ web/src/js/components/Messages.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { usersFindByUsername } from 'utils/users';
 import List from 'material-ui/List';
 import RoomMessage from 'components/RoomMessage';
@@ -11,19 +12,25 @@ class Messages extends React.Component {
     users: PropTypes.object
   };
 
+  componentDidUpdate() {
+    this.scrollRef.scrollToBottom();
+  }
+
   render() {
     const { room, users } = this.props;
 
     return (
-      <List className="up-room__chat__messages">
-        {room.messages.map(message => (
-          <RoomMessage
-            key={message.id}
-            message={message}
-            user={usersFindByUsername(users.repo, message.from)}
-          />
-        ))}
-      </List>
+      <Scrollbars ref={(ref) => { this.scrollRef = ref; }}>
+        <List className="up-room__chat__messages">
+          {room.messages.map(message => (
+            <RoomMessage
+              key={message.id}
+              message={message}
+              user={usersFindByUsername(users.repo, message.from)}
+            />
+          ))}
+        </List>
+      </Scrollbars>
     );
   }
 }
