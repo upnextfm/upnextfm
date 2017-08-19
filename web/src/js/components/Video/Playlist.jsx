fb280d4219d79717eeb4e2514871ecbf02bab0f9
headzoo <sean@headzoo.io>
2017-08-19 11:16:49 -0400

    Updates playlist

--- web/src/js/components/Video/Playlist.jsx
+++ web/src/js/components/Video/Playlist.jsx
@@ -5,20 +5,38 @@ import * as types from 'actions/actionTypes';
 import Button from 'material-ui/Button';
 
 class PlaylistContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      codename: ''
+    };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.codename !== this.props.codename) {
+      this.setState({ codename: nextProps.codename });
+    }
+  }
+
   handleClickPlay = () => {
     const codename = this.inputRef.value;
     this.props.dispatch(playlistPlay(codename, types.PROVIDER_YOUTUBE));
   };
 
+  handleChange = (e) => {
+    this.setState({ codename: e.target.value });
+  };
+
   render() {
-    const { codename } = this.props;
+    const { codename } = this.state;
 
     return (
       <div className="up-room-video__playlist up-paper-container">
         <label htmlFor="up-youtube-id">YouTUBE ID: </label>
         <input
           id="up-youtube-id"
-          defaultValue={codename}
+          value={codename}
+          onChange={this.handleChange}
           style={{ color: 'black' }}
           ref={(ref) => { this.inputRef = ref; }}
         />
