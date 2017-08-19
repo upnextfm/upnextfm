c0cfee9b13c3c5164c4eaea7b4c961079caec473
headzoo <sean@headzoo.io>
2017-08-18 21:19:51 -0400

    Refactoring

--- web/src/js/components/Users.jsx
+++ web/src/js/components/Users.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { usersFindByUsername } from 'utils/users';
 import List, { ListItem } from 'material-ui/List';
-import Hidden from 'material-ui/Hidden';
 import RoomUser from 'components/RoomUser';
 
 class Users extends React.Component {
@@ -16,26 +15,13 @@ class Users extends React.Component {
     const { room, users } = this.props;
 
     return (
-      <div className="up-room__chat__users">
-        <Hidden xsDown>
-          <List>
-            {room.users.map(username => (
-              <ListItem key={username} button>
-                <RoomUser user={usersFindByUsername(users.repo, username)} />
-              </ListItem>
-            ))}
-          </List>
-        </Hidden>
-        <Hidden smUp>
-          <List>
-            {room.users.map(username => (
-              <ListItem key={username} button>
-                <RoomUser user={usersFindByUsername(users.repo, username)} />
-              </ListItem>
-            ))}
-          </List>
-        </Hidden>
-      </div>
+      <List className="up-room__chat__users">
+        {room.users.map(username => (
+          <ListItem key={username} button>
+            <RoomUser user={usersFindByUsername(users.repo, username)} />
+          </ListItem>
+        ))}
+      </List>
     );
   }
 }
