98d1571d28d20acccb77d05408c0ba899cc75159
headzoo <sean@headzoo.io>
2017-08-22 06:25:06 -0400

    Refactoring

--- web/src/js/components/Chat/ChatSide.jsx
+++ web/src/js/components/Chat/ChatSide.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Favico from 'favico.js';
 import { connect } from 'react-redux';
+import { roomJoin } from 'actions/roomActions';
+import { layoutWindowFocused } from 'actions/layoutActions';
+import { domOnWindowBlur } from 'utils/dom';
+import * as api from 'api';
 import UsersPanel from 'components/Chat/UsersPanel';
 import MessagesPanel from 'components/Chat/MessagesPanel';
 import MessageInput from 'components/Chat/MessageInput';
 
 class ChatSide extends React.Component {
+  static propTypes = {
+    roomName:  PropTypes.string.isRequired,
+    socketURI: PropTypes.string.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.favicon = null;
+  }
+
+  componentDidMount() {
+    this.favicon = new Favico();
+    domOnWindowBlur((status) => {
+      this.props.dispatch(layoutWindowFocused(status));
+    });
+
+    api.socket.connect(this.props.socketURI)
+      .then(() => {
+        this.props.dispatch(roomJoin(this.props.roomName));
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.numNewMessages !== this.props.numNewMessages) {
+      this.favicon.badge(this.props.numNewMessages);
+    }
+  }
+
   render() {
     return (
       <div className="up-room-side__chat">
--- web/src/js/components/Room.jsx
+++ web/src/js/components/Room.jsx
@@ -1,59 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Favico from 'favico.js';
 import { connect } from 'react-redux';
-import { roomJoin } from 'actions/roomActions';
-import { layoutWindowFocused } from 'actions/layoutActions';
-import { domOnWindowBlur } from 'utils/dom';
-import * as api from 'api';
 import Hidden from 'material-ui/Hidden';
 import Grid from 'material-ui/Grid';
+import Progress from 'components/Video/Progress';
 import LoginDialog from 'components/Dialogs/LoginDialog';
 import RegisterDialog from 'components/Dialogs/RegisterDialog';
 import ChatSide from 'components/Chat/ChatSide';
 import VideoSide from 'components/Video/VideoSide';
 import VideoNav from 'components/VideoNav';
 import Nav from 'components/Nav';
-import Progress from 'components/Video/Progress';
 
 class Room extends React.Component {
   static propTypes = {
     name:      PropTypes.string.isRequired,
     socketURI: PropTypes.string.isRequired,
     auth:      PropTypes.object.isRequired,
-    layout:    PropTypes.object.isRequired,
-    dispatch:  PropTypes.func
+    layout:    PropTypes.object.isRequired
   };
-
-  static defaultProps = {
-    dispatch: () => {}
-  };
-
-  constructor(props) {
-    super(props);
-    this.favicon = null;
-  }
-
-  componentDidMount() {
-    api.socket.connect(this.props.socketURI)
-      .then(() => {
-        this.props.dispatch(roomJoin(this.props.name));
-      });
-
-    this.favicon = new Favico();
-    domOnWindowBlur((status) => {
-      this.props.dispatch(layoutWindowFocused(status));
-    });
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.room.numNewMessages !== this.props.room.numNewMessages) {
-      this.favicon.badge(this.props.room.numNewMessages);
-    }
-  }
 
   render() {
-    const { auth, layout } = this.props;
+    const { name, socketURI, auth, layout } = this.props;
 
     return (
       <div>
@@ -66,7 +33,7 @@ class Room extends React.Component {
         </Hidden>
         <div className="up-room">
           <Grid item xs={12} sm={12} md={layout.colsChatSide}>
-            <ChatSide />
+            <ChatSide roomName={name} socketURI={socketURI} />
           </Grid>
           <Grid item xs={12} sm={12} md={layout.colsVideoSide}>
             <VideoSide />
@@ -80,7 +47,10 @@ class Room extends React.Component {
 }
 
 function mapStateToProps(state) {
-  return Object.assign({}, state);
+  return {
+    auth:   Object.assign({}, state.auth),
+    layout: Object.assign({}, state.layout)
+  };
 }
 
 export default connect(mapStateToProps)(Room);
