7429492aef5be487668b29669f977b0905b85fcd
headzoo <sean@headzoo.io>
2017-08-22 19:43:46 -0400

    Adds bin/import.php

---
+++ bin/import.php
@@ -0,0 +1,306 @@
+<?php
+use Symfony\Component\Yaml\Yaml;
+use Madcoda\Youtube\Youtube;
+
+include(__DIR__ . "/../vendor/autoload.php");
+$params = Yaml::parse(file_get_contents(__DIR__ . "/../app/config/parameters.yml"));
+$params = $params["parameters"];
+
+$pdoUpnext = new \PDO(
+  "mysql:dbname=${params['database_name']};host=${params['database_host']};charset=utf8",
+  $params["database_user"],
+  $params["database_password"]
+);
+$pdoCytube = new \PDO(
+  "mysql:dbname=${params['database_cytube_name']};host=${params['database_cytube_host']};charset=utf8",
+  $params["database_cytube_user"],
+  $params["database_cytube_password"]
+);
+
+importRooms();
+importVideos();
+importVideoLogs();
+importChatLogs();
+
+/**
+ *
+ */
+function importChatLogs()
+{
+  global $pdoUpnext, $pdoCytube;
+
+  foreach($pdoCytube->query("SELECT * FROM `chat_logs` WHERE `type` = 'message'") as $row) {
+    println($row["user"]);
+
+    $channel = fetchCytubeChannelByID($row["channel_id"]);
+    if (!$channel) {
+      continue;
+    }
+    $room = fetchRoomByName($channel["name"]);
+    if (!$room) {
+      continue;
+    }
+    $user = fetchUserByUsername($row["user"]);
+    if (!$user) {
+      continue;
+    }
+    $message = trim(strip_tags($row["msg"]));
+    if (!$message) {
+      continue;
+    }
+    $meta  = json_decode($row["meta"], true);
+    if (!$meta["color"]) {
+      continue;
+    }
+    $message = sprintf("[%s]%s", $meta["color"], $message);
+
+    $exec = [
+      ":room_id"      => $room["id"],
+      ":user_id"      => $user["id"],
+      ":message"      => $message,
+      ":date_created" => timeToDate($row["time"])
+    ];
+
+    $sql = "
+      INSERT INTO `chat_log`
+      (`room_id`, `user_id`, `message`, `date_created`)
+      VALUES
+      (:room_id, :user_id, :message, :date_created)
+    ";
+    $stmt = $pdoUpnext->prepare($sql);
+    $stmt->execute($exec);
+  }
+}
+
+/**
+ *
+ */
+function importVideoLogs()
+{
+  global $pdoUpnext, $pdoCytube;
+
+  foreach($pdoCytube->query("SELECT * FROM `playlist_history`") as $row) {
+    println($row["channel"] . " " . $row["user"]);
+
+    $stmt = $pdoCytube->prepare("SELECT * FROM `media` WHERE `id` = :id LIMIT 1");
+    $stmt->execute([":id" => $row["media_id"]]);
+    $media = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$media) {
+      continue;
+    }
+
+    $stmt = $pdoUpnext->prepare("SELECT * FROM `video` WHERE `codename` = :codename LIMIT 1");
+    $stmt->execute([":codename" => $media["uid"]]);
+    $video = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$video) {
+      continue;
+    }
+
+    $room = fetchRoomByName($row["channel"]);
+    if (!$room) {
+      continue;
+    }
+    $user = fetchUserByUsername($row["user"]);
+    if (!$user) {
+      continue;
+    }
+
+    $exec = [
+      ":video_id"     => $video["id"],
+      ":room_id"      => $room["id"],
+      ":user_id"      => $user["id"],
+      ":date_created" => timeToDate($row["time"])
+    ];
+
+    $sql = "
+      INSERT INTO `video_log`
+      (`video_id`, `room_id`, `user_id`, `date_created`)
+      VALUES
+      (:video_id, :room_id, :user_id, :date_created)
+    ";
+    $stmt = $pdoUpnext->prepare($sql);
+    $stmt->execute($exec);
+  }
+}
+
+/**
+ *
+ */
+function importVideos()
+{
+  global $pdoUpnext, $pdoCytube, $params;
+
+  $youtube = new Youtube(["key" => $params["app_service_youtube_api_key"]]);
+
+  foreach($pdoCytube->query("SELECT * FROM `media`") as $row) {
+    println($row["title"]);
+    if ($row["type"] == "yt") {
+
+      $stmt = $pdoCytube->prepare("SELECT * FROM `playlist_history` WHERE `media_id` = :media_id ORDER BY `id` ASC LIMIT 1");
+      $stmt->execute([":media_id" => $row["id"]]);
+      $history = $stmt->fetch(PDO::FETCH_ASSOC);
+      if (!$history) {
+        continue;
+      }
+      $stmt = $pdoCytube->prepare("SELECT * FROM `playlist_history` WHERE `media_id` = :media_id ORDER BY `id` DESC LIMIT 1");
+      $stmt->execute([":media_id" => $row["id"]]);
+      $historyLast = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      $createdByUser = fetchUserByUsername($history["user"]);
+      if (!$createdByUser) {
+        continue;
+      }
+      $createdInRoom = fetchRoomByName($history["channel"]);
+      if (!$createdInRoom) {
+        continue;
+      }
+
+      $resp = $youtube->getVideoInfo($row["uid"]);
+      $exec = [
+        ":created_by_user_id" => $createdByUser["id"],
+        ":created_in_room_id" => $createdInRoom["id"],
+        ":permalink"          => "https://youtu.be/${row['uid']}",
+        ":title"              => $resp->snippet->title,
+        ":codename"           => $row["uid"],
+        ":provider"           => "youtube",
+        ":seconds"            => $row["seconds"],
+        ":num_plays"          => fetchCytubePlaylistHistoryCount($history["media_id"]),
+        ":date_created"       => timeToDate($row["time"]),
+        ":date_last_played"   => timeToDate($historyLast["time"]),
+        ":thumb_small"        => (!empty($resp->snippet->thumbnails->medium->url)
+          ? $resp->snippet->thumbnails->medium->url
+          : $resp->snippet->thumbnails->default->url),
+        ":thumb_medium"       => (!empty($resp->snippet->thumbnails->standard->url)
+          ? $resp->snippet->thumbnails->standard->url
+          : $resp->snippet->thumbnails->default->url),
+        ":thumb_large"        => (!empty($resp->snippet->thumbnails->high->url)
+          ? $resp->snippet->thumbnails->high->url
+          : $resp->snippet->thumbnails->default->url)
+      ];
+
+      $sql = "
+      INSERT INTO `video`
+      (`created_by_user_id`, `created_in_room_id`, `permalink`, `title`, `codename`, `provider`, `seconds`, `num_plays`, `date_created`, `date_last_played`, `thumb_small`, `thumb_medium`, `thumb_large`)
+      VALUES
+      (:created_by_user_id, :created_in_room_id, :permalink, :title, :codename, :provider, :seconds, :num_plays, :date_created, :date_last_played, :thumb_small, :thumb_medium, :thumb_large)
+    ";
+      $stmt = $pdoUpnext->prepare($sql);
+      $stmt->execute($exec);
+      usleep(500);
+    }
+  }
+}
+
+/**
+ *
+ */
+function importRooms()
+{
+  global $pdoUpnext, $pdoCytube;
+
+  foreach($pdoCytube->query("SELECT * FROM `channels`") as $row) {
+    println($row["name"]);
+    if ($row["name"] == "lobby") {
+      continue;
+    }
+
+    $owner = fetchUserByUsername($row["owner"]);
+    if (!$owner) {
+      continue;
+    }
+
+    $stmt = $pdoCytube->prepare("SELECT * FROM `channel_data` WHERE `channel_id` = :channel_id AND `key` = 'bio' LIMIT 1");
+    $stmt->execute([":channel_id" => $row["id"]]);
+    $chanBio = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($chanBio) {
+      $chanBio = $chanBio["value"];
+    } else {
+      $chanBio = "";
+    }
+
+    $sql = "
+      INSERT INTO `room`
+      (`name`, `description`, `is_private`, `is_deleted`, `created_by_user_id`, `date_created`)
+      VALUES
+      (:name, :description, :is_private, :is_deleted, :created_by_user_id, :date_created)
+    ";
+    $stmt = $pdoUpnext->prepare($sql);
+    $stmt->execute([
+      ":name"               => $row["name"],
+      ":description"        => $chanBio,
+      ":is_private"         => 0,
+      ":is_deleted"         => 0,
+      ":created_by_user_id" => $owner["id"],
+      ":date_created"       => timeToDate($row["time"])
+    ]);
+  }
+}
+
+/**
+ * @param string $username
+ * @return array
+ */
+function fetchUserByUsername($username)
+{
+  global $pdoUpnext;
+
+  $stmt = $pdoUpnext->prepare("SELECT * FROM `user` WHERE `username` = :username LIMIT 1");
+  $stmt->execute([":username" => $username]);
+  return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+/**
+ * @param string $name
+ * @return array
+ */
+function fetchRoomByName($name)
+{
+  global $pdoUpnext;
+
+  $stmt = $pdoUpnext->prepare("SELECT * FROM `room` WHERE `name` = :name LIMIT 1");
+  $stmt->execute([":name" => $name]);
+  return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+/**
+ * @param int $channelID
+ * @return array
+ */
+function fetchCytubeChannelByID($channelID)
+{
+  global $pdoCytube;
+
+  $stmt = $pdoCytube->prepare("SELECT * FROM `channels` WHERE `id` = :id LIMIT 1");
+  $stmt->execute([":id" => $channelID]);
+  return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+/**
+ * @param int $media_id
+ * @return array
+ */
+function fetchCytubePlaylistHistoryCount($media_id)
+{
+  global $pdoCytube;
+
+  $stmt = $pdoCytube->prepare("SELECT COUNT(*) AS c FROM `playlist_history` WHERE `media_id` = :media_id LIMIT 1");
+  $stmt->execute([":media_id" => $media_id]);
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+  return $row["c"];
+}
+
+/**
+ * @param int $time
+ * @return string
+ */
+function timeToDate($time)
+{
+  return date("Y-m-d H:i:s", $time / 1000);
+}
+
+/**
+ * @param string $str
+ */
+function println($str) {
+  echo $str . "\n";
+}
--- web/src/js/app.jsx
+++ web/src/js/app.jsx
@@ -1,1 +1,15 @@
-console.info('App!');
+$(() => {
+  $('.up-pulse-hover').hover((e) => {
+    let target = $(e.target);
+    if (target.is('.material-icons')) {
+      target = target.parents('.up-pulse-hover');
+    }
+    target.addClass('pulse');
+  }, (e) => {
+    let target = $(e.target);
+    if (target.is('.material-icons')) {
+      target = target.parents('.up-pulse-hover');
+    }
+    target.removeClass('pulse');
+  });
+});
