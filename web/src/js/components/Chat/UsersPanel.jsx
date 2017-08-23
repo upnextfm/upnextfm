357f21f842d80110b241b531b3401fe38585137e
headzoo <sean@headzoo.io>
2017-08-23 19:17:16 -0400

    Adds menu to users list

--- web/src/js/components/Chat/UsersPanel.jsx
+++ web/src/js/components/Chat/UsersPanel.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { usersFindByUsername } from 'utils/users';
 import Hidden from 'material-ui/Hidden';
+import Menu, { MenuItem } from 'material-ui/Menu';
 import List, { ListItem } from 'material-ui/List';
 import IconButton from 'material-ui/IconButton';
 import KeyboardArrowLeft from 'material-ui-icons/KeyboardArrowLeft';
@@ -23,6 +24,25 @@ export default class UsersPanel extends React.Component {
     onCollapse:  () => {}
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: undefined,
+      menuOpen: false
+    };
+  }
+
+  handleClickUser = (e) => {
+    this.setState({
+      menuOpen: true,
+      anchorEl: e.currentTarget
+    });
+  };
+
+  handleCloseMenu = () => {
+    this.setState({ menuOpen: false });
+  };
+
   render() {
     const { roomUsers, repoUsers, isCollapsed, onCollapse } = this.props;
 
@@ -36,7 +56,7 @@ export default class UsersPanel extends React.Component {
       >
         <List>
           {roomUsers.map(username => (
-            <ListItem key={username} button>
+            <ListItem key={username} onClick={this.handleClickUser} button>
               <User user={usersFindByUsername(repoUsers, username)} />
             </ListItem>
           ))}
@@ -54,6 +74,21 @@ export default class UsersPanel extends React.Component {
             </IconButton>
           </div>
         </Hidden>
+        <Menu
+          anchorEl={this.state.anchorEl}
+          open={this.state.menuOpen}
+          onRequestClose={this.handleCloseMenu}
+        >
+          <MenuItem onClick={this.handleCloseMenu}>
+            Private Message
+          </MenuItem>
+          <MenuItem onClick={this.handleCloseMenu}>
+            Profile
+          </MenuItem>
+          <MenuItem onClick={this.handleCloseMenu}>
+            Ignore
+          </MenuItem>
+        </Menu>
       </div>
     );
   }
