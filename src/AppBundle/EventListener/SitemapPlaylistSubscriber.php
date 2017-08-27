7f3f63a5277744b0f4ba0552b70a155da2dfc01a
headzoo <sean@headzoo.io>
2017-08-27 14:38:45 -0400

    Wires up sitemap generator

---
+++ src/AppBundle/EventListener/SitemapPlaylistSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+namespace AppBundle\EventListener;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Presta\SitemapBundle\Event\SitemapPopulateEvent;
+use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+
+class SitemapPlaylistSubscriber
+    implements EventSubscriberInterface
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    /**
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param ObjectManager         $manager
+     */
+    public function __construct(UrlGeneratorInterface $urlGenerator, ObjectManager $manager)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->manager = $manager;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            SitemapPopulateEvent::ON_SITEMAP_POPULATE => 'registerPages',
+        ];
+    }
+
+    /**
+     * @param SitemapPopulateEvent $event
+     */
+    public function registerPages(SitemapPopulateEvent $event)
+    {
+      $event->getUrlContainer()->addUrl(
+        new UrlConcrete(
+          'https://upnext.fm/recent',
+          new \DateTime(),
+          UrlConcrete::CHANGEFREQ_HOURLY
+        ),
+        'playlists'
+      );
+    }
+}
