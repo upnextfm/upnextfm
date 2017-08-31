60e1b68178f05f1373ddfb87f4628c7aabe23979
headzoo <sean@headzoo.io>
2017-08-30 20:45:17 -0400

    Adds up-video-progress-container

--- web/src/js/components/Video/Progress.jsx
+++ web/src/js/components/Video/Progress.jsx
@@ -25,12 +25,14 @@ export default class Progress extends React.Component {
     const percent = Math.floor((time / duration) * 100);
 
     return (
-      <LinearProgress
-        color="primary"
-        mode="determinate"
-        value={isNaN(percent) ? 0 : percent}
-        className="up-video-progress"
-      />
+      <div className="up-video-progress-container">
+        <LinearProgress
+          color="primary"
+          mode="determinate"
+          value={isNaN(percent) ? 0 : percent}
+          className="up-video-progress"
+        />
+      </div>
     );
   }
 }
