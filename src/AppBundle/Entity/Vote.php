20dd33aabad4e0a1b68fd9b2d3c8960fba494390
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-09-21 15:44:13 -0400

    generate the Vote Entity class and stub out its Repository

---
+++ src/AppBundle/Entity/Vote.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vote
+ *
+ * @ORM\Table(name="vote")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\VoteRepository")
+ */
+class Vote
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+      /**
+       * @var \AppBundle\Entity\Video
+       *
+       * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Video", fetch="EAGER")
+       * @ORM\JoinColumns({
+       *   @ORM\JoinColumn(name="video_id", referencedColumnName="id")
+       * })
+       */
+      protected $video;
+
+      /**
+       * @var \AppBundle\Entity\User
+       *
+       * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", fetch="EAGER")
+       * @ORM\JoinColumns({
+       *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+       * })
+       */
+      protected $user;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="value", type="smallint")
+     */
+    private $value;
+
+      /**
+       * @var DateTime
+       *
+       * @ORM\Column(name="date_created", type="datetime", nullable=false)
+       */
+    protected $dateCreated;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param integer $value
+     *
+     * @return Vote
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return int
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+    
+      /**
+       * @return DateTime
+       */
+      public function getDateCreated()
+      {
+        return $this->dateCreated;
+      }
+
+      /**
+       * @param DateTime $dateCreated
+       * @return Favorite
+       */
+      public function setDateCreated(DateTime $dateCreated)
+      {
+        $this->dateCreated = $dateCreated;
+        return $this;
+      }
+}
+
