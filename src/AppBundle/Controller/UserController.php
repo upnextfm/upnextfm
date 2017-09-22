595821f84c561b430009f78e24b9f9e4b3303f6a
headzoo <sean@headzoo.io>
2017-09-22 11:40:22 -0400

    Processes account form

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -1,7 +1,9 @@
 <?php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -14,18 +16,18 @@ class UserController extends Controller
    * @param string $username
    * @return Response
    */
-    public function indexAction($username)
-    {
-        $em     = $this->getDoctrine()->getManager();
-        $user   = $this->findUserOrThrow($username);
-        $events = $em->getRepository("AppBundle:UserEvent")
-        ->findByUser($user, 25);
+  public function indexAction($username)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $user = $this->findUserOrThrow($username);
+    $events = $em->getRepository("AppBundle:UserEvent")
+      ->findByUser($user, 25);
 
-        return $this->render("AppBundle:user:index.html.twig", [
-        "user"   => $user,
-        "events" => $events
-        ]);
-    }
+    return $this->render("AppBundle:user:index.html.twig", [
+      "user"   => $user,
+      "events" => $events
+    ]);
+  }
 
   /**
    * @Route("/u/{username}/favorites/{page}", name="favorites", defaults={"page" = 1})
@@ -34,37 +36,37 @@ class UserController extends Controller
    * @param int $page
    * @return Response
    */
-    public function favoritesAction($username, $page = 1)
-    {
-        $user = $this->findUserOrThrow($username);
+  public function favoritesAction($username, $page = 1)
+  {
+    $user = $this->findUserOrThrow($username);
 
-        $limit  = 30;
-        $offset = ($page - 1) * 30;
+    $limit = 30;
+    $offset = ($page - 1) * 30;
 
-        $em             = $this->getDoctrine()->getManager();
-        $repo           = $em->getRepository("AppBundle:Favorite");
-        $favorites      = $repo->findByUser($user, $limit, $offset);
-        $favoritesCount = $repo->countByUser($user);
+    $em = $this->getDoctrine()->getManager();
+    $repo = $em->getRepository("AppBundle:Favorite");
+    $favorites = $repo->findByUser($user, $limit, $offset);
+    $favoritesCount = $repo->countByUser($user);
 
-        $pages   = ceil($favoritesCount / $limit);
-        $minPage = $page - 4;
-        $maxPage = $page + 4;
-        if ($minPage < 1) {
-            $minPage = 1;
-        }
-        if ($maxPage > $pages) {
-            $maxPage = $pages;
-        }
-
-        return $this->render("AppBundle:user:favorites.html.twig", [
-        "user"           => $user,
-        "favorites"      => $favorites,
-        "favoritesCount" => $favoritesCount,
-        "currentPage"    => $page,
-        "minPage"        => $minPage,
-        "maxPage"        => $maxPage
-        ]);
+    $pages = ceil($favoritesCount / $limit);
+    $minPage = $page - 4;
+    $maxPage = $page + 4;
+    if ($minPage < 1) {
+      $minPage = 1;
     }
+    if ($maxPage > $pages) {
+      $maxPage = $pages;
+    }
+
+    return $this->render("AppBundle:user:favorites.html.twig", [
+      "user"           => $user,
+      "favorites"      => $favorites,
+      "favoritesCount" => $favoritesCount,
+      "currentPage"    => $page,
+      "minPage"        => $minPage,
+      "maxPage"        => $maxPage
+    ]);
+  }
 
   /**
    * @Route("/account", name="account")
@@ -72,35 +74,79 @@ class UserController extends Controller
    * @param Request $request
    * @return Response
    */
-    public function accountAction(Request $request)
-    {
-        $user = $this->getUser();
-        if (!($user instanceof UserInterface)) {
-            throw $this->createNotFoundException();
-        }
-
-        if ($request->getMethod() === "POST") {
-            dump($request->request->all());
-            die();
-        }
-
-        return $this->render("AppBundle:user:account.html.twig", [
-        "user" => $user
-        ]);
+  public function accountAction(Request $request)
+  {
+    /** @var User $user */
+    $user = $this->getUser();
+    if (!($user instanceof UserInterface)) {
+      throw $this->createNotFoundException();
     }
+
+    if ($request->getMethod() === "POST") {
+      $values = $request->request->all();
+      $user->setEmail($values["email"]);
+      $user->setEmailCanonical($values["email"]);
+
+      if (!empty($values["password"])) {
+        $user->setPlainPassword($values["password"]);
+        $this->get("fos_user.user_manager")->updatePassword($user);
+      }
+
+      if ($avatar = $request->files->get("avatar")) {
+        $urls = $this->processAvatar($avatar, $user);
+        $info = $user->getInfo();
+        $info->setAvatarSm($urls["avatarSm"]);
+        $info->setAvatarMd($urls["avatarMd"]);
+        $info->setAvatarLg($urls["avatarLg"]);
+      }
+
+      $this->getDoctrine()->getEntityManager()->flush();
+      $this->addFlash("success", "Accounted updated.");
+    }
+
+    return $this->render("AppBundle:user:account.html.twig", [
+      "user" => $user
+    ]);
+  }
 
   /**
    * @param string $username
    * @return \AppBundle\Entity\User
    */
-    protected function findUserOrThrow($username)
-    {
-        $em   = $this->getDoctrine()->getManager();
-        $user = $em->getRepository("AppBundle:User")->findByUsername($username);
-        if (!$user) {
-            throw $this->createNotFoundException();
-        }
-
-        return $user;
+  protected function findUserOrThrow($username)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $user = $em->getRepository("AppBundle:User")->findByUsername($username);
+    if (!$user) {
+      throw $this->createNotFoundException();
     }
+
+    return $user;
+  }
+
+  /**
+   * @param UploadedFile $avatar
+   * @param User $user
+   * @return array
+   */
+  protected function processAvatar(UploadedFile $avatar, User $user)
+  {
+    $thumbService  = $this->get("app.service.thumbs");
+    $uploadService = $this->get("app.service.upload");
+    $tempFiles     = $thumbService->create($avatar->getPathname());
+    $avatarURLs    = [];
+
+    foreach ($tempFiles as $size => $tempFile) {
+      $avatarName = sprintf("avatar%s", ucwords($size));
+      $uploadName = sprintf("%s/%s/%s", $user->getUsername(), date("Y-m-d"), sprintf("%s-%d.png", $avatarName, rand(0, 1000)));
+      $avatarURLs[$avatarName] = $uploadService->upload(
+        $tempFile,
+        $uploadName,
+        $user,
+        "image/png"
+      );
+    }
+
+    return $avatarURLs;
+  }
 }
